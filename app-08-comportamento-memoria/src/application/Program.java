b139e838fcc36a343c34f7bc5914eb0084ad7877
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2023-03-14 05:53:43 -0300

    Seção #10: Aplicação do conceito de listas no Java.

--- app-08-comportamento-memoria/src/application/Program.java
+++ app-08-comportamento-memoria/src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -14,9 +17,66 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		scanner = new Scanner(System.in);
 		
-		forEach();
+		listas();
 		
 		scanner.close();
+	}
+	
+	public static void listas() {
+		
+		System.out.println("Manipulando Listas no Java.");
+		
+		List<String> nomes = new ArrayList<>();
+		nomes.add("Maria");
+		nomes.add("Alex");
+		nomes.add("Bob");
+		nomes.add("Anna");
+		
+		System.out.println();
+		System.out.println("Imprimindo os nomes adicionados em uma lista com o método add():");
+		imprimirNomes(nomes);
+		
+		System.out.println();
+		System.out.println("Adicionando Marco na posição 2 com o add(posicao, nome):");
+		
+		nomes.add(2, "Marco");
+		imprimirNomes(nomes);
+		System.out.println("Utilizando método size() para obter o tamanho da lista: " + nomes.size());
+		
+		System.out.println();
+		System.out.println("Removendo Anna da lista com o remove(nome):");
+		
+		nomes.remove("Anna");
+		imprimirNomes(nomes);
+		
+		System.out.println();
+		System.out.println("Usando removeIf(predicado) para remover todos os nomes que começam com a letra 'M':");
+		
+		nomes.removeIf(n -> n.charAt(0) == 'M');
+		imprimirNomes(nomes);
+		System.out.println("Usando o indexOf(nome) para encontrar a posição de Bob: " + nomes.indexOf("Bob"));
+		System.out.println("Usando o indexOf(nome) com um nome que não está na lista: " + nomes.indexOf("Marco"));
+		
+		System.out.println();
+		System.out.println("Adicionando novamente todos os nomes na lista:");
+		nomes.add("Maria");
+		nomes.add("Anna");
+		nomes.add("Marco");
+		imprimirNomes(nomes);
+		
+		System.out.println();
+		System.out.println("Filtrando nomes que começam apenas com a letra 'M':");
+		List<String> nomesM = nomes.stream().filter(n -> n.charAt(0) == 'M').collect(Collectors.toList());
+		imprimirNomes(nomesM);
+		
+		System.out.println();
+		System.out.println("Primeiro nome com letra 'B': " + nomes.stream().filter(n -> n.charAt(0) == 'B').findFirst().orElse(null));
+	}
+	
+	private static void imprimirNomes(List<String> nomes) {
+		for (String nome : nomes) {
+			System.out.println("- " + nome);
+		}
 	}
 	
 	public static void forEach() {
